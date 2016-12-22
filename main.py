4f479b2d0cb89f62c6ecf7b92653d36d43c4a8e1
David L <dv.lnh.d@gmail.com>
2016-12-23 00:12:41 +0100

    made index page list all blog posts (not sorted by date yet)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import web
 import markdown
+import os
 
 urls = (
 	'/', 'index',
@@ -10,7 +11,21 @@ app = web.application(urls, globals())
 
 class index:
 	def GET(self):
-		return render.index()
+		# get metadata for posts
+		# [ {title, url, published, description} ] (etc.)
+		meta = []
+		writings = os.listdir('pages/')
+		for post in writings:
+			if post[-3:] != '.md':
+				continue
+			md = markdown.Markdown(extensions = ['markdown.extensions.meta'])
+			f = open("pages/{}".format(post))
+			md.convert(f.read())
+			f.close()
+			md.Meta['url'] = '/writings/{}'.format(post[:-3])
+			meta += [md.Meta]
+		# TODO: sort by date
+		return render.index(meta)
 
 class writing:
 	def GET(self, url):
